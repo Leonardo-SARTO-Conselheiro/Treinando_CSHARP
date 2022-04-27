8d08d536ac807408daf8d34a5871cbf2a32556cf
unknown <leonardocasinha12@gmail.com>
2022-04-27 19:40:12 -0300

    @Leonardo-SARTO-Conselheiro - [Task: Comando para entrada de dados] {27/04/2022 19:40}

---
+++ Entrada_Saida_de_Dados/Entrada_Saida_de_Dados/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entrada_Saida_de_Dados
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //entrada de dados, não quebra a linha//
+            Console.Write("Homer");
+
+            //entrada de dados, quebra de linha//
+            Console.WriteLine("Teste");
+
+            //imprime o codigo do caractere que for digitado(se eu digitar//
+            //A = 65 que é seu codigo numerico, imprimere somente de uma tecla//
+            //int letra = Console.Read();
+            //Console.WriteLine(letra);
+
+            //lê os dados digitados e imprime na tela//
+            string texto = Console.ReadLine();
+            Console.WriteLine(texto);
+
+            //o 1 readkey para imprimir a letra/numero que eu digitar, o 2 readkey encerra o programa//
+            //Console.ReadKey(false);
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
