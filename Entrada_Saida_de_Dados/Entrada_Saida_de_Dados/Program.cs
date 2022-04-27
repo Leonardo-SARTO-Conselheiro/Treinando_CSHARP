b09a6b556324bbdc3e204324c5f17d96aa37e06f
unknown <leonardocasinha12@gmail.com>
2022-04-27 20:03:11 -0300

    @Leonardo-SARTO-Conselheiro - [Task: Saíde de Dados] {27/04/2022 20:05}

--- Entrada_Saida_de_Dados/Entrada_Saida_de_Dados/Program.cs
+++ Entrada_Saida_de_Dados/Entrada_Saida_de_Dados/Program.cs
@@ -10,11 +10,12 @@ namespace Entrada_Saida_de_Dados
     {
         static void Main(string[] args)
         {
+            #region Entrada de Dados
             //entrada de dados, não quebra a linha//
-            Console.Write("Homer");
+            //Console.Write("Homer");
 
             //entrada de dados, quebra de linha//
-            Console.WriteLine("Teste");
+            //Console.WriteLine("Teste");
 
             //imprime o codigo do caractere que for digitado(se eu digitar//
             //A = 65 que é seu codigo numerico, imprimere somente de uma tecla//
@@ -22,14 +23,62 @@ namespace Entrada_Saida_de_Dados
             //Console.WriteLine(letra);
 
             //lê os dados digitados e imprime na tela//
-            string texto = Console.ReadLine();
-            Console.WriteLine(texto);
+            //string texto = Console.ReadLine();
+            //Console.WriteLine(texto);
 
             //o 1 readkey para imprimir a letra/numero que eu digitar, o 2 readkey encerra o programa//
             //Console.ReadKey(false);
-            Console.ReadKey();
+            //Console.ReadKey();
+            #endregion
+
+            #region Saída de Dados
+            //string nome1, nome2, nome3, nome4, auxiliar;
+
+            //A pessoa vai digitar o nome que vai ser atribuido a variavel nome1//
+            //Console.WriteLine("Digite o primeiro nome: ");
+            //nome1 = Console.ReadLine();
+
+            //Console.WriteLine("Digite o segundo nome: ");
+            //nome2 = Console.ReadLine();
+
+            //Console.WriteLine("Digite o terceiro nome: ");
+            //nome3 = Console.ReadLine();
+
+            //Console.WriteLine("Digite o quarto nome: ");
+            //nome4 = Console.ReadLine();
+
+            //vazio, para quebrar a linha//
+            //Console.WriteLine();
+
+            //para mostrar na tela o nome na sequencia//
+            //Console.WriteLine("Nomes inserido na sequência");
+            //Console.WriteLine(nome1);
+            //Console.WriteLine(nome2);
+            //Console.WriteLine(nome3);
+            //Console.WriteLine(nome4);
+
+            //para imprimir os nomes digitados//
+            //Console.ReadKey();
 
 
+            //para mostrar na tela o nome na sequencia invertida//
+            //auxiliar recebeu o valor do nome1//
+            //auxiliar = nome1;
+            //nome1 recebeu o valor do nome4//
+            //nome1 = nome4;
+            //nome4 recebeu o valor do auxiliar, que foi definido na linha 65 que é o valor do nome1//
+            //nome4 = auxiliar;
+            //auxiliar = nome2;
+            //nome2 = nome3;
+            //nome3 = auxiliar;
+
+            //Console.WriteLine(nome1);
+            //Console.WriteLine(nome2);
+            //Console.WriteLine(nome3);
+            //Console.WriteLine(nome4);
+
+            //Console.ReadKey();
+            #endregion
 
         }
     }
